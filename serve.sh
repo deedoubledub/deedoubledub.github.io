6f729407bc3625c14e9fac3c8dd920f4ecef3894
David Wagner <dwagner86@gmail.com>
2022-11-18 14:43:05 -0500

    Revert "resets everything to a clean slate (#11)" (#12)
    This reverts commit 2a0143768dba13df235f42ae4eb5a2889f208e3d.

---
+++ serve.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+docker run --rm -it \
+  --volume="$PWD:/srv/jekyll" \
+  --volume="$PWD/vendor/bundle:/usr/local/bundle" \
+  -p 4000:4000 \
+  jekyll/jekyll:latest \
+  jekyll serve
